474031b3150a6068257f4d407682dcf1fc48eb70
Taylor Williams <taylor@thewilliams.net>
2018-06-12 04:19:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,13 +5,18 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    doc.css(".student-card")
-    binding.pry
+    doc.css(".student-card").map do |student|
+        {
+          :name => student.css(".student-name").text,
+          :location => student.css.(".student-location").text
+          :link => student.children[1].attributes["href"].value
+        }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    
+
   end
 
 end
