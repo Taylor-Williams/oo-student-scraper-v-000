7e22c318dfaf4b3b2b1716c11057c65ca648ea2a
Taylor Williams <taylor@thewilliams.net>
2018-06-12 03:58:56 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,6 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     doc.css(".student-card")
-    end
   end
 
   def self.scrape_profile_page(profile_url)
